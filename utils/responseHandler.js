f755e355f0d8fd755d24865655dc5a83a823f062
DavidDevGt <josuedavidvl18@gmail.com>
2023-11-09 13:22:37 -0600

    Refactor code to improve readability and performance.

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,174 +1,203 @@
-/**
- * La función "success" envía una respuesta exitosa con los datos proporcionados y el código de estado.
- * 
- * :param res: El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Por lo general, lo proporciona el marco web o la biblioteca que está utilizando
- * :param data: El parámetro "datos" son los datos de respuesta que desea enviar al cliente. Puede ser
- * cualquier objeto o valor de JavaScript que desee incluir en el cuerpo de la respuesta
- * :param code: El parámetro "código" es un parámetro opcional que representa el código de estado HTTP
- * que se enviará en la respuesta. Si no se proporciona, el valor predeterminado es 200 (OK), defaults
- * to 200 (optional)
- */
-function sucess(res, data, code = 200) {
-  res.status(code).json({
+class CustomError extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+  }
+}
+
+const errors = {
+  badRequest: {
+    status: 400,
+    message:
+      "La solicitud es incorrecta o está mal formada. Verifique los datos enviados e intente nuevamente.",
+  },
+  unauthorized: {
+    status: 401,
+    message:
+      "No autorizado. Debe autenticarse para obtener la respuesta solicitada.",
+  },
+  paymentRequired: {
+    status: 402,
+    message:
+      "Pago requerido. La solicitud no se puede procesar hasta que se realice el pago.",
+  },
+  forbidden: {
+    status: 403,
+    message:
+      "Prohibido. No tiene los permisos necesarios para acceder a este recurso.",
+  },
+  notFound: {
+    status: 404,
+    message: "El recurso solicitado no fue encontrado.",
+  },
+  methodNotAllowed: {
+    status: 405,
+    message:
+      "Método no permitido. No se puede utilizar este método para la solicitud realizada.",
+  },
+  notAcceptable: {
+    status: 406,
+    message:
+      "No aceptable. El servidor no puede generar un contenido acorde a los Accept headers enviados en la petición.",
+  },
+  proxyAuthenticationRequired: {
+    status: 407,
+    message:
+      "Se requiere autenticación de proxy para continuar con la solicitud.",
+  },
+  requestTimeout: {
+    status: 408,
+    message:
+      "Tiempo de espera de la solicitud agotado. El servidor cerró la conexión por inactividad.",
+  },
+  conflict: {
+    status: 409,
+    message:
+      "Conflicto. La solicitud no se pudo procesar debido a un conflicto con el estado actual del recurso.",
+  },
+  gone: {
+    status: 410,
+    message:
+      "Gone. El recurso solicitado ya no está disponible y no lo estará en el futuro.",
+  },
+  lengthRequired: {
+    status: 411,
+    message:
+      "Longitud requerida. La solicitud no especificó la longitud de su contenido, que es requerida por el recurso solicitado.",
+  },
+  preconditionFailed: {
+    status: 412,
+    message:
+      "Precondición fallida. Una o más condiciones dadas en los headers de la solicitud fallaron al evaluarse en el servidor.",
+  },
+  payloadTooLarge: {
+    status: 413,
+    message:
+      "Carga útil demasiado grande. La solicitud es mayor de lo que el servidor está dispuesto o puede procesar.",
+  },
+  unsupportedMediaType: {
+    status: 415,
+    message:
+      "Tipo de medio no soportado. La solicitud tiene un formato que no es soportado por el recurso solicitado.",
+  },
+  rangeNotSatisfiable: {
+    status: 416,
+    message:
+      "Rango no satisfactorio. El cliente ha solicitado una porción del archivo, pero el servidor no puede suministrar esa parte.",
+  },
+  expectationFailed: {
+    status: 417,
+    message:
+      "Expectativa fallida. El servidor no puede cumplir con los requerimientos del campo Expect de la cabecera de la solicitud.",
+  },
+  teapot: {
+    status: 418,
+    message:
+      "Soy una tetera. La solicitud fue dirigida a una tetera y no puede hacer café.",
+  },
+  misdirectedRequest: {
+    status: 421,
+    message:
+      "Solicitud mal dirigida. La solicitud fue dirigida a un servidor que no es capaz de producir una respuesta.",
+  },
+  unprocessableEntity: {
+    status: 422,
+    message:
+      "Entidad no procesable. La solicitud está bien formada, pero no se pudo seguir debido a errores semánticos.",
+  },
+  locked: {
+    status: 423,
+    message: "Bloqueado. El recurso al que se está accediendo está bloqueado.",
+  },
+  failedDependency: {
+    status: 424,
+    message:
+      "Dependencia fallida. La solicitud falló debido a una falla en una solicitud previa.",
+  },
+  tooEarly: {
+    status: 425,
+    message:
+      "Demasiado temprano. La solicitud puede ser repetida, pero podría ser rechazada por precaución.",
+  },
+  upgradeRequired: {
+    status: 426,
+    message:
+      "Se requiere actualización. El cliente debería cambiar a un protocolo diferente tal como se indica en el campo Upgrade de la cabecera.",
+  },
+  preconditionRequired: {
+    status: 428,
+    message:
+      "Precondición requerida. El servidor requiere que la solicitud del navegador sea condicional.",
+  },
+  tooManyRequests: {
+    status: 429,
+    message:
+      "Demasiadas solicitudes. El usuario ha enviado demasiadas solicitudes en un período de tiempo dado.",
+  },
+  requestHeaderFieldsTooLarge: {
+    status: 431,
+    message:
+      "Campos de cabecera de solicitud demasiado grandes. El servidor no está dispuesto a procesar la solicitud porque uno o más campos de cabecera son demasiado grandes.",
+  },
+  unavailableForLegalReasons: {
+    status: 451,
+    message:
+      "No disponible por razones legales. El contenido ha sido retirado como consecuencia de una orden judicial o demanda relacionada con derechos de autor.",
+  },
+  internalServerError: {
+    status: 500,
+    message:
+      "Error interno del servidor. Ocurrió un error inesperado y el servidor no puede completar la solicitud.",
+  },
+  notImplemented: {
+    status: 501,
+    message:
+      "No implementado. El servidor no soporta una funcionalidad necesaria para responder a la solicitud.",
+  },
+  badGateway: {
+    status: 502,
+    message:
+      "Mal gateway. El servidor estaba actuando como un gateway o proxy y recibió una respuesta inválida del servidor ascendente.",
+  },
+  serviceUnavailable: {
+    status: 503,
+    message:
+      "Servicio no disponible. El servidor no está listo para manejar la solicitud, usualmente porque está sobrecargado o en mantenimiento.",
+  },
+  gatewayTimeout: {
+    status: 504,
+    message:
+      "Tiempo de espera del gateway agotado. El servidor estaba actuando como un gateway o proxy y no recibió una respuesta a tiempo del servidor ascendente.",
+  },
+  httpVersionNotSupported: {
+    status: 505,
+    message:
+      "Versión HTTP no soportada. La versión de HTTP utilizada en la solicitud no es soportada por el servidor.",
+  },
+  // Puedes continuar añadiendo más errores según lo necesites...
+};
+
+function responseHandler(res, data, status = 200) {
+  res.status(status).json({
     error: false,
-    status: code,
+    status: status,
     body: data,
   });
 }
 
-/**
- * La función "failure" envía una respuesta JSON con un mensaje de error y un código de estado al
- * cliente.
- * 
- * :param res: El parámetro "res" es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Normalmente lo proporciona el marco web o la biblioteca que se utiliza
- * :param message: El parámetro de mensaje es una cadena que representa el mensaje de error que se
- * devolverá en el cuerpo de la respuesta
- * :param code: El parámetro "código" es un parámetro opcional que representa el código de estado HTTP
- * que se enviará en la respuesta. Si no se proporciona, el valor predeterminado es 500 (Error interno
- * del servidor), defaults to 500 (optional)
- */
-function failure(res, message, code = 500) {
-  res.status(code).json({
+function errorHandler(res, error) {
+  let errorResponse = {
     error: true,
-    status: code,
-    body: message,
-  });
-}
+    status: error.status || 500,
+    message: error.message || errors.internalServerError.message,
+  };
 
-/**
- * La función `notFound` es una función auxiliar en JavaScript que envía una respuesta JSON con un
- * código de estado 404 y un mensaje de error predeterminado de "No encontrado".
- * 
- * :param res: El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Normalmente es una instancia de la clase `http.ServerResponse` en Node.js o un objeto
- * equivalente en otros marcos o bibliotecas
- * :param message: El parámetro "mensaje" es un parámetro opcional que representa el mensaje de error
- * que se devolverá en el cuerpo de la respuesta. Si no se proporciona ningún "mensaje", se utilizará
- * el valor predeterminado "No encontrado", defaults to Not Found (optional)
- */
-function notFound(res, message = "Not Found") {
-  res.status(404).json({
-    error: true,
-    status: 404,
-    body: message,
-  });
-}
-
-/**
- * La función `badRequest` es una función auxiliar en JavaScript que envía una respuesta JSON con un
- * código de estado 400, un mensaje de error y datos opcionales.
- * 
- * :param res: El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Normalmente lo proporciona el marco web o la biblioteca que se utiliza
- * :param message: El parámetro `message` es una cadena que representa el mensaje de error que se
- * enviará en la respuesta. Es opcional y tiene un valor predeterminado de "Solicitud incorrecta" si no
- * se proporciona, defaults to Bad Request (optional)
- * :param data: El parámetro "datos" es un parámetro opcional que representa cualquier dato adicional
- * que desee incluir en el cuerpo de la respuesta. Es un objeto que puede contener cualquier par
- * clave-valor que desee enviar al cliente (optional)
- */
-function badRequest(res, message = "Bad Request", data = {}) {
-    res.status(400).json({
-        error: true,
-        status: 400,
-        message: message,
-        body: data,
-    });
-} 
-
-/**
- * La función "unauthorized" envía una respuesta JSON con un código de estado 401 y un mensaje de
- * error que indica acceso no autorizado.
- * 
- * :param res: El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Normalmente es una instancia de la clase `http.ServerResponse` en Node.js o un objeto
- * equivalente en otros marcos
- * :param message: El parámetro `message` es un parámetro opcional que representa el mensaje de error
- * que se enviará en la respuesta. Si no se proporciona ningún mensaje, se utilizará el mensaje
- * predeterminado "No autorizado", defaults to Unauthorized (optional)
- */
-function unauthorized(res, message = "Unauthorized") {
-    res.status(401).json({
-        error: true,
-        status: 401,
-        message: message,
-    });
-}
-
-/**
- * La función "forbidden" envía una respuesta 403 Prohibido con un mensaje de error opcional.
- * 
- * :param res: El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Normalmente es una instancia de la clase `http.ServerResponse` en Node.js o un objeto
- * equivalente en otros marcos
- * :param message: El parámetro "mensaje" es un parámetro opcional que representa el mensaje de error
- * que se incluirá en la respuesta. Si no se proporciona ningún "mensaje", el valor predeterminado se
- * establece en "Prohibido", defaults to Forbidden (optional)
- */
-function forbidden(res, message = "Forbidden") {
-    res.status(403).json({
-        error: true,
-        status: 403,
-        message: message,
-    });
-}
-
-/**
- * La función "conflict" es una función auxiliar en JavaScript que envía una respuesta de conflicto
- * 409 con un mensaje de error y datos opcionales.
- * 
- * :param res: El parámetro `res` es el objeto de respuesta que se utiliza para enviar la respuesta al
- * cliente. Suele ser una instancia de la clase `Response` en un marco web como Express.js
- * :param message: El parámetro de mensaje es una cadena que representa el mensaje de error que se
- * devolverá en la respuesta. Es opcional y tiene un valor predeterminado de "Conflicto" si no se
- * proporciona, defaults to Conflict (optional)
- * :param data: El parámetro "datos" es un parámetro opcional que representa cualquier dato adicional
- * que desee incluir en el cuerpo de la respuesta. Es un objeto que puede contener cualquier par
- * clave-valor que desee enviar al cliente (optional)
- */
-function conflict(res, message = "Conflict", data = {}) {
-    res.status(409).json({
-        error: true,
-        status: 409,
-        message: message,
-        body: data,
-    });
-}
-
-/**
- * La función "created" se utiliza para enviar una respuesta exitosa con datos y un código de estado de
- * 201.
- * 
- * :param res: El parámetro "res" se utiliza normalmente para representar el objeto de respuesta en una
- * aplicación web. Se utiliza para enviar la respuesta al cliente
- * :param data: El parámetro "datos" son los datos que se enviarán como respuesta al cliente. Puede ser
- * cualquier tipo de datos, como un objeto, una matriz o una cadena
- */
-function created(res, data) {
-    sucess(res, data, 201);
-}
-
-/**
- * La función "noContent" envía una respuesta con un código de estado de 204, indicando que no hay
- * contenido para enviar.
- * 
- * :param res: El parámetro `res` es un objeto que representa la respuesta HTTP que se enviará al
- * cliente
- */
-function noContent(res) {
-    res.status(204).send();
+  res.status(errorResponse.status).json(errorResponse);
 }
 
 module.exports = {
-    success,
-    failure,
-    notFound,
-    badRequest,
-    unauthorized,
-    forbidden,
-    conflict,
-    created,
-    noContent,
-  };
+  responseHandler,
+  errorHandler,
+  CustomError,
+  errors,
+};
